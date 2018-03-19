ca7034ec2333f4ce9dbe2fc8196cbd704f015219
Zlkshb <zulkifli.muchammad@gmail.com>
2018-03-19 10:49:29 +0700

    change app.py

--- app.py
+++ app.py
@@ -26,8 +26,3 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
